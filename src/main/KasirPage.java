672b04d85496ea7af3799450b623291fed95dd0c
DIYAS <DIYAS@LenovoIP314ITL6>
2025-01-16 05:32:28 +0700

    komit37

--- src/main/KasirPage.java
+++ src/main/KasirPage.java
@@ -227,6 +227,7 @@ public class KasirPage extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
